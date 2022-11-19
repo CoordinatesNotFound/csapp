3584414dfcdaca7df0a142d9dbb363cd3b23bb5e
yinanhu519@gmail.com <OliverForest_>
2022-11-19 16:09:03 +0800

    2022-11-19

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
 now=$(date "+%Y-%m-%d")
-echo "Change Directory to D:\Desktop\TechStack\CSAPP"
-cd D:\Desktop\TechStack\CSAPP
 echo "Starting add-commit-pull-push..."
 git add -A && git commit -m "$now" && git pull && git push
 echo "Finish!"
